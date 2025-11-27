69d39ed3c073aea9a234701ee168692d366185fe
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 16:49:04 +0700

    menambahkan Entry View Model

---
+++ app/src/main/java/com/example/teori8/viewmodel/EntryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.teori8.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.teori8.repositori.RepositorySiswa
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel(){
+
+}
