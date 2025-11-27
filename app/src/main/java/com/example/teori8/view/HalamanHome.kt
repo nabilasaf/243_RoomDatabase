837cd8a950b865c1fe12175c8434e3638667128b
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 17:15:58 +0700

    Menambahkan Halaman Home

--- app/src/main/java/com/example/teori8/view/HalamanHome.kt
+++ app/src/main/java/com/example/teori8/view/HalamanHome.kt
@@ -2,39 +2,17 @@ package com.example.teori8.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.teori8.viewmodel.HomeViewModel
+import com.example.teori8.viewmodel.provider.PenyediaViewModel
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
-fun EntrySiswaScreen(
-    navigateBack: () -> Unit,
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    Scaffold (
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            SiswaTopAppBar(
-                title = stringResource(id = DestinasiEntry.titleRes),
-                canNavigateBack = true,
-                scrollBehavior = scrollBehavior
-            )
-        }
-    ) { innerPadding ->
-        EntrySiswaBody(
-            uiStateSiswa = viewModel.uiStateSiswa,
-            onSiswaValueChange = viewModel::updateUiState,
-            onSaveClick = {
-                coroutineScope.launch {
-                    viewModel.saveSiswa()
-                    navigateBack()
-                }
-            },
-            modifier = Modifier
-                .padding(paddingValues = innerPadding)
-                .verticalScroll(state = rememberScrollState())
-                .fillMaxWidth()
-        )
-    }
+
 }
