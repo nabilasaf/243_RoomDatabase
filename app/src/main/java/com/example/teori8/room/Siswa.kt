4eeba9eb0519beecfe091c8fc08ad33032e4f25e
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 09:14:02 +0700

    menambahkan class siswa

---
+++ app/src/main/java/com/example/teori8/room/Siswa.kt
@@ -0,0 +1,13 @@
+package com.example.teori8.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "siswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
