9fd4b1ca7f312a41bb0dc660094129437e6328f3
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 17:30:48 +0700

    Menambahkan Peta Navigasi

---
+++ app/src/main/java/com/example/teori8/view/uicontroller/PetaNavigas.kt
@@ -0,0 +1,36 @@
+package com.example.teori8.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.teori8.view.HomeScreen
+import com.example.teori8.view.route.DestinasiEntry
+import com.example.teori8.view.route.DestinasiHome
+
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = modifier)
+    {
+        composable (route = DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(route = DestinasiEntry.route)},
+            )
+        }
+        composable (route = DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
+
