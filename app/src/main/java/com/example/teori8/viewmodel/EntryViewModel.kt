2d7a4df6fbf05be94b990f774653420bdd92a958
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 16:59:58 +0700

    menambahkan Entry View Model

--- app/src/main/java/com/example/teori8/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/teori8/viewmodel/EntryViewModel.kt
@@ -1,8 +1,34 @@
 package com.example.teori8.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.teori8.repositori.RepositorySiswa
 
-class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel(){
+class EntryViewModel(private val repositoriSiswa: RepositorySiswa): ViewModel() {
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(value = UiStateSiswa())
+        private set
 
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UiStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(uiState = detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(siswa = uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
 }
