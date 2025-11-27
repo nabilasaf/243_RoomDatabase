f9b5e2a0d6110e5d5fcde0fa828d6f2ccfdb36ee
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 17:36:06 +0700

    Menambahkan entry siswa body di Halaman Entry

--- app/src/main/java/com/example/teori8/view/HalamanEntry.kt
+++ app/src/main/java/com/example/teori8/view/HalamanEntry.kt
@@ -1,22 +1,32 @@
 package com.example.teori8.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.teori8.view.route.DestinasiEntry
+import com.example.teori8.viewmodel.DetailSiswa
 import com.example.teori8.viewmodel.EntryViewModel
+import com.example.teori8.viewmodel.UiStateSiswa
 import com.example.teori8.viewmodel.provider.PenyediaViewModel
 import kotlinx.coroutines.launch
+import com.example.teori8.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -51,6 +61,33 @@ fun EntrySiswaScreen(
                 .verticalScroll(state = rememberScrollState())
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UiStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = 20.dp)),
+        modifier = modifier.padding(all = dimensionResource(id = 16.dp))
+    ) {
+        FormInputSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(id = R.string.btn_submit))
+        }
     }
 }
 
