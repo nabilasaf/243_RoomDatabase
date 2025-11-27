6ec185719b6a50d1e37f4c8c5b06c363f74762ce
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 17:03:35 +0700

    menambahkan Detail Siswa

---
+++ app/src/main/java/com/example/teori8/view/HalamanEntry.kt
@@ -0,0 +1,8 @@
+package com.example.teori8.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
