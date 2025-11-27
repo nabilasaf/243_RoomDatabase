f86e58d07e64cab49fa0a03530f035a9a7d69407
nabilasaf <nabilasafitri861@gmail.com>
2025-11-27 17:23:59 +0700

    Menambahkan AppBar

---
+++ app/src/main/java/com/example/teori8/view/AppBar.kt
@@ -0,0 +1,29 @@
+package com.example.teori8.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(text = title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource("Back")
+                    )
+                }
+            }
+        }
+    )
+}
