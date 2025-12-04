8d8cc282c07e673dbb1e5f070b9eb50f1c59a639
nabilasaf <nabilasafitri861@gmail.com>
2025-12-04 10:11:42 +0700

    menambahkan repo update

--- app/src/main/java/com/example/teori8/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/teori8/repositori/RepositoriSiswa.kt
@@ -19,6 +19,6 @@ class OfflineRepositorySiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-//    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 
 }
