4243b5e1dce77edca7e2de08d4699a71390f60d3
nabilasaf <nabilasafitri861@gmail.com>
2025-12-04 09:59:44 +0700

    menambahkan destinasi edit siswa

---
+++ app/src/main/java/com/example/teori8/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.teori8.view.route
+
+import com.example.teori8.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+}
