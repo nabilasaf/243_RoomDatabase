1cbef17a7931bec13bc7801acea759f67e00382d
nabilasaf <nabilasafitri861@gmail.com>
2025-12-04 09:42:17 +0700

    menambahkan data di Repositori

--- app/src/main/java/com/example/teori8/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/teori8/repositori/RepositoriSiswa.kt
@@ -8,6 +8,8 @@ interface RepositorySiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun insertSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)
+//    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositorySiswa(
@@ -16,4 +18,7 @@ class OfflineRepositorySiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+//    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+
 }
